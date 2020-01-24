c9ec9c0de1c9128de18993035718a3d2df31795e
Abshir Mohamed <abshirtech24@gmail.com>
2020-01-24 13:36:56 -0800

    algorithm homework 2 added

---
+++ algorithmhw2.js
@@ -0,0 +1,15 @@
+//POWER FUNCTION
+
+function power(num){
+    return Math.pow(num,2)
+}
+
+console.log(power(2))
+
+//String length function
+
+function stringlength(string){
+    return string.length;
+}
+
+console.log(stringlength("ABC"))
