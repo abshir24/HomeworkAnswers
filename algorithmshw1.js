90230d7f242df4244fe6e7676f43f22a6d15e3bf
Abshir Mohamed <abshirtech24@gmail.com>
2020-01-24 13:35:06 -0800

    added algorithmw1 answers

---
+++ algorithmshw1.js
@@ -0,0 +1,31 @@
+//Loop that prints all values from 1-100
+
+for(let i = 1;i<=100;i++){
+    console.log(i)
+}
+
+// Loop that prints all even values from 1-100
+
+for(let i = 1;i<=100;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+
+// Loop that prints all even values from 1-100
+
+for(let i = 1;i<=100;i++){
+    if(i%2==1){
+        console.log(i)
+    }
+}
+
+// Fizz Buzz for loop
+
+for(let i = 1;i<=100;i++){
+    if(i%2==0){
+        console.log("FIZZ")
+    }else{
+        console.log("BUZZ")
+    }
+}
